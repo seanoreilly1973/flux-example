7c1283aec0a95a2be2d3a268eae8eb83038e689e
seanoreilly1973 <sean.oreilly@linux.com>
2018-10-28 21:48:29 +0000

    Update helloworld.go

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -12,7 +12,7 @@ var rev = "devel"
 func main() {
 	var (
 		addr = flag.String("addr", ":80", "HTTP listen address")
-		msg  = flag.String("msg", "Hello, Lucy", "Message to print")
+		msg  = flag.String("msg", "Hello, Lucy. How are you?", "Message to print")
 	)
 	flag.Parse()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
